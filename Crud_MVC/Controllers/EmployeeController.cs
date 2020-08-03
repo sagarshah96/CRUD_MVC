dfd1783912c42377b86b1050049dfe155ad39fcc
Sagar <Sagar@SAGAR>
2020-08-04 01:13:19 +0530

    Add Datatable

---
+++ Crud_MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using Crud_MVC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud_MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetData()
+        {
+            try
+            {
+                using (DBModel db = new DBModel())
+                {
+                    List<Employee> empList = db.Employees.ToList<Employee>();
+                    return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        [HttpGet]
+        public ActionResult AddOrEdit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            return View();
+        }
+    }
+}
