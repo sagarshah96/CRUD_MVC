25a29cce8e1a7384e5102edcc179a353e24a3f3e
Sagar <Sagar@SAGAR>
2020-08-05 00:03:13 +0530

    Add - Display

--- Crud_MVC/Controllers/EmployeeController.cs
+++ Crud_MVC/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using Crud_MVC.Data;
+using Crud_MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,9 +25,9 @@ namespace Crud_MVC.Controllers
                     return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
                 }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                throw;
+                throw ex;
             }
         }
         [HttpGet]
@@ -35,9 +36,28 @@ namespace Crud_MVC.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult AddOrEdit(Employee emp)
+        public ActionResult AddOrEdit(ModelEmployee memp)
         {
-            return View();
+            try
+            {
+                using (DBModel db = new DBModel())
+                {
+                    Employee emp = new Employee();
+                    emp.Name = memp.Name;
+                    emp.Position = memp.Position;
+                    emp.Office = memp.Office;
+                    emp.Age = memp.Age;
+                    emp.Salary = memp.Salary;
+                    db.Employees.Add(emp);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }            
         }
     }
 }
---
+++ Crud_MVC/Models/ModelEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Crud_MVC.Models
+{
+    public class ModelEmployee
+    {
+        public int EmployeeId { get; set; }
+        [Required(ErrorMessage ="This Field is Required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "This Field is Required")]
+        public string Position { get; set; }
+        public string Office { get; set; }
+        public Nullable<int> Age { get; set; }
+        public Nullable<int> Salary { get; set; }
+    }
+}
