2316de18b6618da928caec9dfdb6c089cd4a52ed
Sagar <Sagar@SAGAR>
2020-08-05 23:27:58 +0530

    Complete crud

--- Crud_MVC/Controllers/EmployeeController.cs
+++ Crud_MVC/Controllers/EmployeeController.cs
@@ -21,7 +21,7 @@ namespace Crud_MVC.Controllers
             {
                 using (DBModel db = new DBModel())
                 {
-                    List<Employee> empList = db.Employees.ToList<Employee>();
+                    List<Employee> empList = db.Employees.ToList();
                     return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
                 }
             }
@@ -31,9 +31,31 @@ namespace Crud_MVC.Controllers
             }
         }
         [HttpGet]
-        public ActionResult AddOrEdit()
+        public ActionResult AddOrEdit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return View();
+            }
+            else
+            {
+                using (DBModel db = new DBModel())
+                {
+                    ModelEmployee emp = new ModelEmployee();
+
+                    emp = db.Employees.Where(x => x.EmployeeId == id).Select(x => new ModelEmployee
+                    {
+                        EmployeeId = x.EmployeeId,
+                        Name = x.Name,
+                        Position = x.Position,
+                        Office = x.Office,
+                        Age = x.Age,
+                        Salary = x.Salary
+                    }).FirstOrDefault();
+                    return View(emp);
+
+                }
+            }
         }
         [HttpPost]
         public ActionResult AddOrEdit(ModelEmployee memp)
@@ -43,21 +65,58 @@ namespace Crud_MVC.Controllers
                 using (DBModel db = new DBModel())
                 {
                     Employee emp = new Employee();
-                    emp.Name = memp.Name;
-                    emp.Position = memp.Position;
-                    emp.Office = memp.Office;
-                    emp.Age = memp.Age;
-                    emp.Salary = memp.Salary;
-                    db.Employees.Add(emp);
-                    db.SaveChanges();
-                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                    if (memp.EmployeeId == 0)
+                    {
+                        emp.Name = memp.Name;
+                        emp.Position = memp.Position;
+                        emp.Office = memp.Office;
+                        emp.Age = memp.Age;
+                        emp.Salary = memp.Salary;
+                        db.Employees.Add(emp);
+                        db.SaveChanges();
+                        return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                    }
+                    else
+                    {
+                        var empp = db.Employees.Find(memp.EmployeeId);
+                        empp.Name = memp.Name;
+                        empp.Position = memp.Position;
+                        empp.Office = memp.Office;
+                        empp.Age = memp.Age;
+                        empp.Salary = memp.Salary;
+                        db.SaveChanges();
+                        return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+                    }
+
                 }
             }
             catch (Exception ex)
             {
-
                 throw ex;
-            }            
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (DBModel db = new DBModel())
+                {
+                    Employee emp = new Employee();
+                    emp = db.Employees.Find(id);
+                    db.Employees.Remove(emp);
+                    db.SaveChanges();
+                    return Json(new { success = true,message = "Successfully Delete..!"},JsonRequestBehavior.AllowGet);
+                }
+                    
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
         }
     }
 }
